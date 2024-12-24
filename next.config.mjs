ad113140bffcdad31655c41e3cf9418437de7ab5
Yana <yana-kim-2002@mail.ru>
2024-12-24 17:03:47 +0300

    gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  assetPrefix: isProd ? "/next-store" : "",
+  basePath: isProd ? "/next-store" : "",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
